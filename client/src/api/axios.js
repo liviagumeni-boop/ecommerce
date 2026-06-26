43eb5852566fde0e617a084182de6e55eb0a80e8
liviagumeni07 <liviagumeni@gmail.com>
2026-06-26 15:56:21 +0200

    .

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://ecommerce-x4el.onrender.com/api",  // hardcoded temporarily
   headers: {
     "Content-Type": "application/json",
   },
