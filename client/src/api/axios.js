1ce4c7bd6593e8f246aba9bc69fa378f1c60090a
liviagumeni07 <liviagumeni@gmail.com>
2026-06-26 16:32:33 +0200

    add api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecommerce-366p.onrender.com",  // hardcoded temporarily
+  baseURL: "https://ecommerce-366p.onrender.com/api",  // hardcoded temporarily
   headers: {
     "Content-Type": "application/json",
   },
