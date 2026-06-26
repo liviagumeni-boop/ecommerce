baa71f54d6247ddedecfbab10b4b00a891c614a1
liviagumeni07 <liviagumeni@gmail.com>
2026-06-26 17:12:29 +0200

    .

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const corsOptions = {
   origin: "https://ecommerce-qhno.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "userid"],
 };
 
 app.use(cors(corsOptions));
