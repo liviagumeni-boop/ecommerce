2fb0f54178af49b1a0ca017988b4fbddf291395d
Livia Gumeni <liviagumeni@gmail.com>
2026-07-07 12:51:33 +0200

    .

---
+++ client/src/componets/ui/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type LoadingProps = {
+  message?: string;
+};
+
+const Loading: React.FC<LoadingProps> = ({ message = "Loading..." }) => {
+  return <div className="p-4">{message}</div>;
+};
+
+export default Loading;
